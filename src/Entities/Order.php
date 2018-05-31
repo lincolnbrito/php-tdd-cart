793deef68c7577f27bd80a226b4d31ed1dd0aeea
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-05-31 13:56:19 -0300

    order test

--- src/Entities/Order.php
+++ src/Entities/Order.php
@@ -3,5 +3,39 @@ namespace LincolnBrito\Cart\Entities;
 
 class Order
 {
-    
+    protected $products;
+
+    public function __construct(){
+        $this->products = new \ArrayObject();
+    }
+
+     /**
+     * @return mixed
+     */
+    public function getProducts()
+    {
+        return $this->products;
+    }
+
+    /**
+     * @param \ArrayObject $product
+     */
+    public function addProduct(ProductInterface $product)
+    {
+        $this->products->append($product);
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotal():int
+    {
+        $total = 0;
+
+        foreach($this->products as $product) {
+            $total += $product->getPrice();
+        }
+
+        return $total;
+    }
 }
--- tests/Entities/OrderTest.php
+++ tests/Entities/OrderTest.php
@@ -3,8 +3,47 @@ namespace LincolnBrito\Cart\Tests\Entities;
 
 use PHPUnit\Framework\TestCase;
 use LincolnBrito\Cart\Entities\Order;
+use LincolnBrito\Cart\Entities\Product;
 
-class OrderTest
+class OrderTest extends TestCase
 {
-    
+    public function testGetProductList()
+    {
+        $product = new Product();
+        $product->setName("Product 1");
+        $product->setDescription("Desc 1");
+        $product->setPrice(10);
+
+        $product2 = new Product();
+        $product2->setName("Product 2");
+        $product2->setDescription("Desc 2");
+        $product2->setPrice(20);
+
+        $order = new Order();
+        $order->addProduct($product);
+        $order->addProduct($product2);
+
+        $products = new \ArrayObject([$product, $product2]);
+        $this->assertEquals($products, $order->getProducts());
+    }
+
+    public function testGetTotal()
+    {        
+        $product = new Product();
+        $product->setName("Product 1");
+        $product->setDescription("Desc 1");
+        $product->setPrice(10);
+
+        $product2 = new Product();
+        $product2->setName("Product 2");
+        $product2->setDescription("Desc 2");
+        $product2->setPrice(20);
+
+        $order = new Order();
+        $order->addProduct($product);
+        $order->addProduct($product2);
+
+        $total = 30;
+        $this->assertEquals($total, $order->getTotal());
+    }
 }
