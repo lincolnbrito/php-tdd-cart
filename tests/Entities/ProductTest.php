abd2f03a41146d70e9bbeba4a5151d5264db45c1
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-05-31 13:13:11 -0300

    create product test

--- tests/Entities/ProductTest.php
+++ tests/Entities/ProductTest.php
@@ -4,6 +4,12 @@ namespace LincolnBrito\Cart\Tests\Entities;
 
 use PHPUnit\Framework\TestCase;
 
-class ProductTest extends TestCase {
+class ProductTest extends TestCase 
+{
+    public function testProductType()
+    {
+        $product = new Product();
 
+        $this->assertInstanceOf(ProductInterface::class, $product);
+    }
 }
